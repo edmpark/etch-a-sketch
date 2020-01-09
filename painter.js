642027acbc50149a99bcd646a695888029be0ac4
Edmond P <edmondpark1@gmail.com>
2020-01-08 18:32:02 -0800

    added in button doesn't do anything yet

--- painter.js
+++ painter.js
@@ -8,3 +8,11 @@ for(var i = 0; i<16*16; i++){
     container.innerHTML+=createDivTemplate('gridItem');
 }
 
+function newGrid(width, height){
+
+}
+
+document.getElementById('clearButton').onClick = newGrid(window.prompt('How many squares wide'), 
+window.prompt('How many squares tall') ); 
+
+
