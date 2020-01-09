a0a77354dc3d146f906c9cb2ad432bd3c3f96410
Edmond P <edmondpark1@gmail.com>
2020-01-08 18:05:22 -0800

    Got grid to color when mouseover

--- painter.js
+++ painter.js
@@ -1,10 +1,10 @@
 var container = document.getElementById('container');
 
 function createDivTemplate(dClass){
-    return '<div class="' + dClass + '"> </div>';
+    return '<div class="' + dClass + '" onmouseover="this.style.background=\'gray\' "> </div>';
 }
 
 for(var i = 0; i<16*16; i++){
-    container.innerHTML+=createDivTemplate('grid-item');
+    container.innerHTML+=createDivTemplate('gridItem');
 }
 
