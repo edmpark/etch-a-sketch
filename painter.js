4b872a20af2640b6aeefbc89233ee08291bf6836
Edmond P <edmondpark1@gmail.com>
2020-01-09 22:20:18 -0800

    done with new grid button

--- painter.js
+++ painter.js
@@ -1,18 +1,42 @@
-var container = document.getElementById('container');
+let container = document.getElementById('container');
+let containerClass=document.getElementsByClassName('container')[0];
+let pixelNum=0;
 
 function createDivTemplate(dClass){
     return '<div class="' + dClass + '" onmouseover="this.style.background=\'gray\' "> </div>';
 }
 
-for(var i = 0; i<16*16; i++){
-    container.innerHTML+=createDivTemplate('gridItem');
+createGridItems(16);
+
+var numSquare;
+function promptMe(){
+    numSquare=window.prompt('How many squares per side?');
+    createGridItems(numSquare);
 }
 
-function newGrid(width, height){
-
+function deleteAllGridItem(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
 
-document.getElementById('clearButton').onClick = newGrid(window.prompt('How many squares wide'), 
-window.prompt('How many squares tall') ); 
+function createGridItems(sideSquareNum){
+    deleteAllGridItem();
+    pixelNum=900/sideSquareNum;
+    document.documentElement.style.setProperty("--gridLength", sideSquareNum);
+    for(var i = 0; i<sideSquareNum*sideSquareNum; i++){
+        container.innerHTML+=createDivTemplate('gridItem');
+    }
+}
+
+function newGrid(gridWidth, gridHeight){
+    deleteAllGridItem();
+    container.style.gridTemplateColumns="";
+    let rowAmount = 723/gridHeight;
+    container.style.setProperty('grid-template-columns', 'repeat('+gridWidth+', minmax(auto, auto))');
+    createGridItems(gridWidth, gridHeight);
+}
+
+
 
 
